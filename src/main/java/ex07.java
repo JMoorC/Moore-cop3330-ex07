8c5b2075fa89106dddb00e6fe6bfa7f310a9d4e7
Jim <jcmoore@knights.ucf.edu>
2021-09-12 00:49:48 -0400

    Initial commit

---
+++ src/main/java/ex07.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 James Moore
+ */
+
+import java.util.Scanner;
+import static java.lang.Float.parseFloat;
+
+public class ex07 {
+    static final double MTF2 = 0.09290304;
+    public static void main(String[] args){
+        Scanner inScan = new Scanner(System.in);
+
+        System.out.print("What is the length of the room in feet? ");
+        double length = parseFloat(inScan.nextLine());
+        System.out.print("What is the width of the room in feet? ");
+        double width = parseFloat(inScan.nextLine());
+        double area = length * width;
+
+        System.out.printf(
+                "You entered dimensions of %.3f feet by %.3f feet\nThe area is\n%.3f square feet\n%.3f square meters\n"
+                ,length, width, area, area * mtf2);
+    }
+}
